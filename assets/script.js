8e508cd0fba1283ad7a70a718e301f5b25148db2
Jacob Banks <kobybanks@gmail.com>
2021-06-18 10:31:32 -0400

    filled html from js

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,57 @@
-const now = moment().hour();
-const day = moment().format("dddd, MMMM Do YYYY");
-let schedTask = [];
+const setTargetHour = moment().hour() + 1; //find the next hour
+const nextHour = moment().hour(setTargetHour).minute(0).second(0); // save next hour
 
-$("#currentDay").text(day);
-$(".time-block").each(function (index) {
-  if (now == this.id) {
-    $(this).children("textarea").addClass("present");
-    $(this).children("textarea").removeClass("past");
-    $(this).children("textarea").removeClass("future");
-  } else if (now > this.id) {
-    $(this).children("textarea").addClass("past");
-    $(this).children("textarea").removeClass("present");
-    $(this).children("textarea").removeClass("future");
-  } else {
-    $(this).children("textarea").addClass("future");
-    $(this).children("textarea").removeClass("past");
-    $(this).children("textarea").removeClass("present");
+const day = moment().format("dddd, MMMM Do YYYY"); // get current time
+let schedTask = []; //empty array for saved todos
+let merideim = ""; //
+let time = 9; //start time
+
+$("#currentDay").text(day); //set the date at top of page
+
+// fill time blocks with 9 blocks
+for (let i = 0; i < 9; i++) {
+  //set 12 hour clock
+  if (time > 12) {
+    time = 1;
   }
-});
+  //set whether am or pm
+  if (time > 8) {
+    meridiem = "AM";
+  } else {
+    meridiem = "PM";
+  }
+  //html content for time blocks //set the id to the 24 hour moment hours//
+  let textBlock = `<div id="${i + 9}" class="row time-block">
+                   <div class="col-2 col-md-1 hour"><p class="my-3">${time}${meridiem}</p></div>
+                   <textarea class="col-8 col-md-10 description"></textarea>
+                   <button class="btn saveBtn col-2 col-md-1"><i class="far fa-save fa-2x"></i></button></div>`;
+  $(".container").append(textBlock);
+  time++;
+}
 
+// add the class that controls todo task despcription background
+const addTimeClass = function () {
+  $(".time-block").each(function (index) {
+    const now = moment().hour();
+    if (now == this.id) {
+      $(this).children("textarea").addClass("present");
+      $(this).children("textarea").removeClass("past");
+      $(this).children("textarea").removeClass("future");
+    } else if (now > this.id) {
+      $(this).children("textarea").addClass("past");
+      $(this).children("textarea").removeClass("present");
+      $(this).children("textarea").removeClass("future");
+    } else {
+      $(this).children("textarea").addClass("future");
+      $(this).children("textarea").removeClass("past");
+      $(this).children("textarea").removeClass("present");
+    }
+  });
+};
+
+addTimeClass(); // run
+
+// save  tasks description to local storage
 $(".saveBtn").on("click", function () {
   //get id
   let time = $(this).parent().attr("id");
@@ -30,9 +63,20 @@ $(".saveBtn").on("click", function () {
   localStorage.setItem(`${time}`, value);
 });
 
+// go through time blocks get id // check storage for saved task fill description if theres a value
 $(".time-block").each(function () {
   let timeId = $(this).attr("id");
   $(this)
     .children("textarea")
     .val(localStorage.getItem(`${timeId}`));
 });
+
+// refresh description every hour
+const refreshTaskClass = function () {
+  addTimeClass();
+  let min = 60 * (1000 * 60);
+  console.log("test");
+  setTimeout(refreshTaskClass, min);
+};
+// refresh description background once a new hour has been reached
+setTimeout(refreshTaskClass, nextHour.diff(moment()));
